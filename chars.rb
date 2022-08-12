09e08aab1873db4d74de4939c41b8392c55f1c5e
ErikBoesen <me@erikboesen.com>
2022-08-11 18:35:22 -0700

    Add chars file

---
+++ chars.rb
@@ -0,0 +1,11 @@
+class Chars
+  DOGS = ["🥰", "😊", "☺️", "🥺", "😇"]
+  DOG_DECORATORS = ["💕", "💓", "🦴", "🎾", "🥏"]
+  ENVIRONMENT_DECORATORS = ["🛝"", "🍃", "🍂", "🌲", "🌳", "🦴", "🎾", "🥏"]
+  IDEOGRAPHIC_SPACE = "\u3000"
+  EN_SPACE = "\u2002"
+  EM_SPACE = "\u2003"
+  THREE_PER_EM_SPACE = "\u2004"
+  THIN_SPACE = "\u2009"
+  HAIR_SPACE = "\u200a"
+end

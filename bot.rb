48ffef875fdb43adaad7b523577d6db4bacb3019
ErikBoesen <me@erikboesen.com>
2022-08-15 18:37:41 -0700

    Add kneecapped bot script

---
+++ bot.rb
@@ -0,0 +1,16 @@
+require 'json'
+require 'twitter'
+require './generator'
+
+credentials = JSON.parse(File.read('credentials.json'))
+
+client = Twitter::REST::Client.new do |config|
+    config.consumer_key = credentials['consumer_key']
+    config.consumer_secret = credentials['consumer_secret']
+    config.access_token = credentials['access_token']
+    config.access_token_secret = credentials['access_token_secret']
+end
+
+text = generate()
+#client.update(text)
+puts "Tweeted:\n#{text}"
